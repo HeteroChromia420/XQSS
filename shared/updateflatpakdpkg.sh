7eba6b4466d8ff01ea78f7ba0ba232140f1aacf3
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2024-02-13 01:54:55 +0000

    Update updateflatpakdpkg.sh

--- shared/updateflatpakdpkg.sh
+++ shared/updateflatpakdpkg.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#Update Script that is run via package update indicator, updates both flatpak and dpkg packages whenever there's a dpkg package update available. normally i would not want to use a full blown script for this kind of thing, but my hand got forced on that regard.
-#The only reason i even have to do this is because of the run dialog for package-update-indicator not allowing to run more than one command. this is really silly otherwise. Thanks, i guess...
+# Check if the user has sudo privileges
+if sudo -l &> /dev/null; then
+    echo "User has sudo privileges. Running 'sudo apt dist-upgrade --auto-remove'."
+    sudo apt dist-upgrade --auto-remove
+else
+    echo "User does not have sudo privileges. Skipping 'sudo apt dist-upgrade --auto-remove'."
+fi
 
-#Get to the meat of the script already, dammit!
-
-sudo apt dist-upgrade --auto-remove --no-install-recommends
-flatpak update
+# Run flatpak update for user
+echo "Running 'flatpak update --user'."
+flatpak update --user
